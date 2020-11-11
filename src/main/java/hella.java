ee75b270d5dff8de918337fcc118bb8c7e123a77
慶崋 <wqh86868686@163.com>
2020-11-11 17:37:29 +0800

    调整之后提交

--- src/main/java/hella.java
+++ src/main/java/hella.java
@@ -2,5 +2,6 @@ public class hella {
     public static void main(String[] args) {
         System.out.println("args666666 = " + args);
         System.out.println("args888888 = " + args);
+        System.out.println("868686 = " + args);
     }
 }
